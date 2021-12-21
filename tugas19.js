9e9acfee9afc5f9600fae14901134a5c3aedce8b
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2021-12-21 22:18:04 +0700

    Pengumpulan Tugas 19 JS - NIOMIC

---
+++ tugas19.js
@@ -0,0 +1,15 @@
+console.log("REPEAT\n=========================");
+const teks = "Semangat...";
+console.log(teks.repeat(5));
+
+console.log("\n");
+const bintang = "*****";
+console.log(bintang.repeat(1));
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(9));
+console.log(bintang.repeat(13));
+console.log(bintang.repeat(19));
+console.log(bintang.repeat(13));
+console.log(bintang.repeat(9));
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(1));
